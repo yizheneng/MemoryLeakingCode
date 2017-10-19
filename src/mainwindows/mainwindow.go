b41060b676d5b25c956a7c8bc226f9bd4c1edb06
yizheneng <kennalee@163.com>
2017-10-19 10:47:52 +0800

    [dev] 重现内存溢出问题

--- src/mainwindows/mainwindow.go
+++ src/mainwindows/mainwindow.go
@@ -3,6 +3,7 @@ package main
 import (
 	. "widgets"
 
+	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/widgets"
 )
 
@@ -14,9 +15,19 @@ type MainWindow struct {
 func NewMainWindow() *MainWindow {
 	mainWindow := &MainWindow{}
 	mainWindow.QWidget = widgets.NewQWidget(nil, 0)
-	mainWindow.title = NewTitle()
+	mainWindow.title = NewTitle(mainWindow.QWidget)
+	mainWindow.SetWindowFlags(core.Qt__FramelessWindowHint)
+	mainWindow.SetMinimumSize2(800, 600)
+
+	mainWidget := widgets.NewQWidget(nil, 0)
+	mainWidget.SetObjectName("mainWidget")
+
+	layout := widgets.NewQVBoxLayout()
+	layout.AddWidget(mainWindow.title, 1, 0)
+	layout.AddWidget(mainWidget, 5, 0)
 
 	mainWindow.SetWindowTitle("起重机监控中心")
 	mainWindow.SetObjectName("MainWindow")
+	mainWindow.SetLayout(layout)
 	return mainWindow
 }
--- src/widgets/title.go
+++ src/widgets/title.go
@@ -1,18 +1,54 @@
 package widgets
 
 import (
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 )
 
 type Title struct {
 	*widgets.QWidget
+	mainWindows *widgets.QWidget
+
+	mouseFlag bool
+	windowPos *core.QPoint
+	mousePos  *core.QPoint
 }
 
-func NewTitle() *Title {
+func NewTitle(mainWindows *widgets.QWidget) *Title {
 	title := &Title{}
 	title.QWidget = widgets.NewQWidget(nil, 0)
+	title.mainWindows = mainWindows
+	title.mouseFlag = false
+	title.windowPos = nil
+	title.mousePos = nil
 
 	title.SetObjectName("Title")
+
+	title.ConnectMousePressEvent(func(event *gui.QMouseEvent) {
+		title.mouseFlag = true
+
+		if title.windowPos != nil {
+			title.windowPos.DestroyQPoint()
+		}
+
+		if title.mousePos != nil {
+			title.mousePos.DestroyQPoint()
+		}
+
+		title.windowPos = mainWindows.Pos()
+		title.mousePos = event.GlobalPos()
+	})
+
+	title.ConnectMouseReleaseEvent(func(event *gui.QMouseEvent) {
+		title.mouseFlag = false
+	})
+
+	title.ConnectMouseMoveEvent(func(event *gui.QMouseEvent) {
+		if title.mouseFlag {
+			mainWindows.Move2(title.windowPos.X()+event.GlobalPos().X()-title.mousePos.X(), title.windowPos.Y()+event.GlobalPos().Y()-title.mousePos.Y())
+		}
+	})
 
 	return title
 }
